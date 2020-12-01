252a8d5b5880d6d01be666bbf1b8c1de80e497d5
Sven2301 <svegra01@gmail.com>
2020-11-30 19:50:54 -0600

    Flower Poblation v0.5

--- flor.py
+++ flor.py
@@ -1,10 +1,110 @@
 #FLOR
 
+# La posición constará de 14 bits,
+# En tras palabras, 2 cadenas de 7 bits cada una
+# esto para generar numero entre 0 y 127
+# ya que el tamaño de la matriz de flores es de 127x127
+
+# El color constará de 3 bits, ya que son suficientes
+# para hacer 8 combinaciones que son la cantidad total
+# colores que podrán tener las flores
+
+# Los primeros 7 bits son de las posicion i,
+# los siguientes 7 bits son de la posicion j
+# y los siguientes 3 bits son del color
+# Estos estarán en el DNA
+
 class Flor:
-    def __init__(self, color, dna, pos):
-        self.color = color
+    
+    def __init__(self, dna):
+        
         self.dna = dna
-        self.pos = pos
+        self.color = (0, 0, 0)
+        self.pos = (63, 63)
+        self.index = (0, 0)
+
+
+    def decodePos(dna):
+
+        genPosI = []
+        genPosJ = []
+
+        for i in range (0, 7):
+
+            genPosI += dna[i]
+
+        for i in range (7, 14):
+
+            genPosJ += dna[i]
+
+        binI = int(''.join(map(str,genPosI)))
+        binJ = int(''.join(map(str,genPosj)))
+
+        I = binaryToDecimal(binX)
+        J = binaryToDecimal(binY)
+
+        self.index[0] = I
+        self.index[1] = J
+
+        self.pos = indexToAxis(I, J)
+            
+
+    
+    def decodeColor(dna):
+
+        genColor = []
+
+        for i in range(14, 17):
+
+            genColor.append(dna[i])
+
+        for i in range(0, len(genColor)):
+
+            if genColor[i]:
+
+                self.color[i] = 255
+
+        print(self.color)
+
+            
+    def binaryToDecimal(binary): 
+          
+        binary1 = binary 
+        decimal, i, n = 0, 0, 0
+        while(binary != 0): 
+            dec = binary % 10
+            decimal = decimal + dec * pow(2, i) 
+            binary = binary//10
+            i += 1
+        return decimal
+
+
+    def indexToAxis(i, j):
+
+        if i < 64:
+
+            X = -(63 - i)
+
+        else:
+
+            X = i - 64
+
+        if j < 64:
+
+            Y = 63 - j
+
+        else:
+
+            Y = -(i - 63)
+
+        return (X, Y)
+        
+
+        
+
+    
+
+        
         
     
-        
+
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #PROGRA DE ABEJAS Y  FLORES
 # Imports
 import pygame
+from random import randint
+from flor import Flor
 
 
 pygame.init()
+
+
 #CORREGIR LA MATRIZ CON LAS STRUCTS
 # E: Dos ints
 # S: Una Matriz
@@ -15,20 +19,25 @@ def generadorMatriz(filas, columnas):
         M.append(columna)
 
     return M
+
+
 #MATRIZ
 mapa = generadorMatriz(128,128)
+
+
 #POBLACION INICIAL
 poblacionAbejas = []
 poblacionFlores = []
+
+
 #CONVERSOR
 #E: Un numero
 #S: Un numero binario
 #D: Convierte el numero dado en binario
-def toBin(n):
-    if n == 0:
-        return ''
-    else:
-        return toBin(n//2) + str(n%2)
+def decimalToBinary(n):  
+    return bin(n).replace("0b", "")
+
+    
 #CREADOR DE ABEJAS
 """
 ESTRUCTURA:
@@ -39,10 +48,51 @@ ESTRUCTURA:
 #S:
 #D:
 
-#CREADOR DE FLORES
-#E:
-#S:
-#D:
+def genFlowerPoblation():
+
+    for i in range(0, 127):
+
+        for j in range(0, 127):
+
+            row = []
+            if i == 63 and j == 63:
+
+
+                row.append(Flor([]))
+                continue
+            
+            else:
+                
+                dna = []
+                n = randint(0, 8)
+                genI = decimalToBinary(i)
+                genJ = decimalToBinary(j)
+                genColor = decimalToBinary(n)
+
+                for bit in genI:
+
+                    dna.append(bit)
+
+                for bit in genJ:
+
+                    dna.append(bit)
+
+                for bit in genColor:
+
+                    dna.append(bit)
+
+                row.append(Flor(dna))
+
+        poblacionFlores.append(row)
+
+
+    for row in poblacionFlores:
+
+        for flor in row:
+
+            print(flor.dna)
+
+                       
 
 
 #GUI
